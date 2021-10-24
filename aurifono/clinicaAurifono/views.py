ead683cccb09443b9af11c26d76e62f2b8b69e37
Jessica Caroline <jessicarolyne@hotmail.com>
2021-10-24 04:55:07 -0300

    Desenvolvimento do menu principal

--- aurifono/clinicaAurifono/views.py
+++ aurifono/clinicaAurifono/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
  
 def index(request):
-    return render(request, 'clinicaAurifono/cadastro.html')
+    return render(request, 'clinicaAurifono/index.html')
 
