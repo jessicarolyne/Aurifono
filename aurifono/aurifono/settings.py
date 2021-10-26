dbf0fc64db40c918a743ac8e12b2fcbb7841b72c
Jessica Caroline <jessicarolyne@hotmail.com>
2021-10-25 23:17:41 -0300

    Acessando mysql e criando o models

--- aurifono/aurifono/settings.py
+++ aurifono/aurifono/settings.py
@@ -76,11 +76,16 @@ WSGI_APPLICATION = 'aurifono.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clinica',
+        'USER': 'root',
+        'PASSWORD':'sis50819',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- aurifono/clinicaAurifono/models.py
+++ aurifono/clinicaAurifono/models.py
@@ -1,21 +1,63 @@
 from django.db import models
+from django.db.models.base import Model
 from django.db.models.fields import TextField
 
-class clinicaAurifono(models.Model):
+class profissionalenc_profissionalenc(models.Model):
     STATUS = (
-        ('fazer', 'Fazer'),
-        ('pronto', 'Pronto')
+        ('1','Ativo'),
+        ('0','Inativo')
     )
-
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    done = models.CharField(
-        max_length=6,
-        # select
-        choices=STATUS,
+    nome = models.CharField(max_length=100)
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS    
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
-
     def __str__(self):
-        return self.title
+        return "{} ({})".format(self.nome)
+
+class paciente_paciente(models.Model):
+    SEXO = (
+        ('feminino', 'Feminino'),
+        ('masculino', 'Masculino'),
+    )
+    RG = models.CharField(max_length=12)
+    CPF = models.CharField(max_length=14)
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    DataNascimento = models.DateField(verbose_name='Data de Nascimento')
+    DataCadastro = models.DateTimeField(auto_now_add=True, verbose_name='Data do cadastro')
+    DataAtualizacao = models.DateTimeField(auto_now=True, verbose_name='Data da ultima atualização')
+    sexo = models.CharField(
+        max_length=9,
+        choices=SEXO,
+        verbose_name='Sexo'
+    )
+    Profissional_id = models.ForeignKey(profissionalenc_profissionalenc, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return "{} ({})".format(self.nome, self.DataNascimento)
+
+class avaliacaoad_avaliacaoad(models.Model):
+    ORELHA = (
+        ('ES', 'ESQUERDA'),
+        ('DI', 'DIREITA')
+    )
+    DataAvaliacao = models.DateField()
+    QOrelha = models.CharField(
+        max_length=2,
+        choices=ORELHA
+    )
+    paciente_id = models.ForeignKey(paciente_paciente, on_delete=models.PROTECT)
+    def __str__(self):
+        return self.DataAvaliacao
+
+class comunicoral_comunicoral(models.Model):
+    dsc_habilidade = models.CharField(max_length=150)
+    def __str__(self):
+        return self.dsc_habilidade
+
+class comunicoralidade_comunicoralidade(models.Model):
+    resp_oralidade = models.CharField(max_length=1)
+    ComunicOral_id = models.ForeignKey(comunicoral_comunicoral, on_delete=models.PROTECT)
+    avaliacaoAD_id = models.ForeignKey(avaliacaoad_avaliacaoad, on_delete=models.PROTECT)
+    def __str__(self):
+        return self.resp_oralidade
