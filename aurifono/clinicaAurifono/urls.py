5c74174a1cb38f6fdbbcbd680e95253bbc5418b5
Jessica Caroline <jessicarolyne@hotmail.com>
2021-10-26 22:58:34 -0300

    Criando a página Lista de Pacientes

--- aurifono/clinicaAurifono/urls.py
+++ aurifono/clinicaAurifono/urls.py
@@ -4,3 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+urlpatterns = [
+    path('', views.buscaPaciente, name='buscaPaciente'),
+]
+
--- aurifono/clinicaAurifono/views.py
+++ aurifono/clinicaAurifono/views.py
@@ -1,7 +1,14 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import paciente_paciente
  
+def buscaPaciente(request):
+    pacientes = paciente_paciente.objects.all() 
+    return render(request, 'clinicaAurifono/buscaPaciente.html', {'pacientes' : pacientes})
+    
 def index(request):
     return render(request, 'clinicaAurifono/index.html')
+
 
